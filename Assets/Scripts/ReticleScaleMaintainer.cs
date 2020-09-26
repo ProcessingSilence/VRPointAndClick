56e17dc1c915d7295199b6aa37edf6224fa69db6
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-25 23:33:41 -0400

    11:33 PM

---
+++ Assets/Scripts/ReticleScaleMaintainer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ReticleScaleMaintainer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale != Testing.GlobalReticleScale.globalScale)
+        {
+            transform.localScale = Testing.GlobalReticleScale.globalScale;
+        }
+    }
+}
--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -5,47 +5,62 @@ using UnityEngine.UI;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
+// TO DO: Rename Testing.cs to ShrinkGrow.cs
 public class Testing : MonoBehaviour
 {
     public Transform player;
 
     public XRRig XRRig_Script;
 
-    private float currentYOffset;
     private float lineLength;
     private Vector3 currentReticleScale;
     
     private bool _primaryButtonAction;
 
     public GameObject rightControllerObj, leftControllerObj;
-    private XRInteractorLineVisual _rightLine, _leftLine;
+    private XRInteractorLineVisual _rightray, _leftray;
     private XRController _rightController, _leftController;
-    private bool _shrinkOrGrow;
+    private LineRenderer _rightLine, _leftLine;
 
-    //public bool triggerVal;
+    private float originalLineWidth;
+   
+    private bool _shrinkOrGrow;
     private bool primaryVal;
     private bool primaryInput;
-    private bool shrinkOrGrow;
+    private bool shrinkOrGrow = true;
 
     public GameObject reticlePrefab;
     
+    private Vector3 bigScale = Vector3.one;
+    private Vector3 tinyScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+    public Transform mainCamera;
+    private Vector3 originalCameraRotation;
+
+    public static class GlobalReticleScale
+    {
+        public static Vector3 globalScale;
+    }
+
     // Start is called before the first frame update
     void Awake()
     {
-        _rightLine = rightControllerObj.GetComponent<XRInteractorLineVisual>();
+        _rightray = rightControllerObj.GetComponent<XRInteractorLineVisual>();
         _rightController = rightControllerObj.GetComponent<XRController>();
+        _rightLine = rightControllerObj.GetComponent<LineRenderer>();
         
-        _leftLine = leftControllerObj.GetComponent<XRInteractorLineVisual>();
+        _leftray = leftControllerObj.GetComponent<XRInteractorLineVisual>();
         _leftController = leftControllerObj.GetComponent<XRController>();
+        _leftLine = leftControllerObj.GetComponent<LineRenderer>();
+
+        originalLineWidth = _rightLine.startWidth;
         
         XRRig_Script = player.GetComponent<XRRig>();
-        
-        currentYOffset = XRRig_Script.cameraYOffset;
-        
-        lineLength = _rightLine.lineLength;
 
-        currentReticleScale = reticlePrefab.transform.localScale;
-        _rightLine.reticle = _leftLine.reticle = reticlePrefab;
+        lineLength = _rightray.lineLength;
+
+        currentReticleScale = reticlePrefab.transform.localScale = new Vector3(1, .05f, 1);
+        _rightray.reticle = _leftray.reticle = reticlePrefab;
     }
 
     // Update is called once per frame
@@ -59,24 +74,24 @@ public class Testing : MonoBehaviour
 
     private void Shrink()
     {
-        player.localScale = new Vector3(0.2f,0.2f,0.2f);
+        player.localScale = tinyScale;
         //XRRig_Script.cameraYOffset = currentYOffset/10;
-        _rightLine.lineLength = _leftLine.lineLength = lineLength * 10;
-        _rightLine.reticle.transform.localScale =
-            _leftLine.reticle.transform.localScale = currentReticleScale/10;
+        _rightray.lineLength = _leftray.lineLength = lineLength * 10;
+        _rightray.reticle.transform.localScale =
+            _leftray.reticle.transform.localScale = GlobalReticleScale.globalScale = currentReticleScale/10;
+        _rightLine.startWidth = _rightLine.endWidth = _leftLine.startWidth = _leftLine.endWidth = originalLineWidth/10; 
     }
 
     private void Grow()
     {
-        player.localScale = new Vector3(1f,1f,1f);
+        player.localScale = bigScale;
         //XRRig_Script.cameraYOffset = currentYOffset;
-        _rightLine.lineLength = _leftLine.lineLength = lineLength;
-        _rightLine.reticle.transform.localScale =
-            _leftLine.reticle.transform.localScale = currentReticleScale;
+        _rightray.lineLength = _leftray.lineLength = lineLength;
+        _rightray.reticle.transform.localScale =
+            _leftray.reticle.transform.localScale = GlobalReticleScale.globalScale = currentReticleScale;
+        _rightLine.startWidth = _rightLine.endWidth = _leftLine.startWidth = _leftLine.endWidth = originalLineWidth;
     }
 
-    // Why is all of this required to press one. simple. button.
-    // WHY.
     private void PrimaryInputPress()
     {
         primaryInput = false;
@@ -102,38 +117,34 @@ public class Testing : MonoBehaviour
     // Activated upon release of primary button.
     private void ShrinkOrGrowSwap()
     {
-        /*
-        // Get press down
-        if (primaryVal && primaryIsUpFlag == 0)
-        {
-            primaryIsUpFlag = 1;
-        }
-
-        
-        if (primaryVal == false && primaryIsUpFlag == 1)
-        {
-            primaryIsUpFlag = 2;
-            shrinkOrGrow = !shrinkOrGrow;
-        }
-*/
         if (!primaryInput && primaryVal)
         {
             shrinkOrGrow = !shrinkOrGrow;
-            InputTracking.Recenter();
+            RecenterRotation();
         }
-        if (shrinkOrGrow == false)
+        
+        if (shrinkOrGrow == false && player.localScale != tinyScale)
         {
             Shrink();
+            Debug.Log("Shrunk");
         }
 
-        if (shrinkOrGrow)
+        if (shrinkOrGrow && player.localScale != bigScale)
         {
             Grow();
+            Debug.Log("Grown");
         }
-
-
     }
 
+    // Using InputTrackingRecenter resets the angle each time it's used. 
+    // Gets camera y angle, and has XR Rig face towards that y angle upon Primary being pressed.
+    private void RecenterRotation()
+    {
+        originalCameraRotation = mainCamera.eulerAngles;
+        InputTracking.Recenter();
+        player.eulerAngles = new Vector3(player.eulerAngles.x, originalCameraRotation.y, player.eulerAngles.z);
+    }
+    
 
     /*
      private void TriggerInputDetection()
