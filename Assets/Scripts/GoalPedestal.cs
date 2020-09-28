1ed8b4023424ef882bf5f137f0ca082ea7d18b28
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-27 20:43:00 -0400

    8:42 PM

---
+++ Assets/Scripts/GoalPedestal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+
+public class GoalPedestal : MonoBehaviour
+{
+    public XRSocketInteractor XrSocketInteractor_script;
+
+    private bool loadingScene;
+
+    public string nextScene;
+
+    // Activated via XRSocketInteractor.cs 
+    public void LoadScene()
+    {
+        if (loadingScene == false)
+        {
+            loadingScene = true;
+            StartCoroutine(Delay());
+        }
+    }
+
+    private IEnumerator Delay()
+    {
+        loadingScene = true;
+        XrSocketInteractor_script.enabled = false;
+        yield return new WaitForSecondsRealtime(0.5f);
+        SceneManager.LoadScene(nextScene);
+    }    
+}
