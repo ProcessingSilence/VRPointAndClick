f4e9020d2786b89bd02d1e1a62d28cbab515fdd6
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-30 19:08:10 -0400

    7:07 PM

---
+++ Assets/Scripts/OrangeParentPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeParentPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+            transform.parent = other.transform;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+            transform.parent = null;
+    }
+}
---
+++ Assets/Scripts/RespawnObj.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObj : MonoBehaviour
+{
+    private Vector3 respawnPos;
+
+    private Rigidbody _rb; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        respawnPos = transform.position;
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -15)
+        {
+            transform.position = respawnPos;
+            _rb.velocity = Vector3.zero;
+        }
+    }
+}
