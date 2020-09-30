6ba39a8ae0ce9a4b9638d00347e83f525a853833
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-29 20:54:43 -0400

    8:54 PM

---
+++ Assets/Scripts/BecomeChildToPlatform.cs
@@ -0,0 +1,44 @@
+﻿// Become a child of any object as long as they are tagged as "Platform", correct scale to always be (1,1,1) in world space.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BecomeChildToPlatform : MonoBehaviour
+{
+    public Transform platform;
+    public Transform platformParent;
+
+    public XRBaseInteractor rightController, leftController;
+
+    private bool decideIfParent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rightController.onSelectExit.AddListener(GetPlatform);
+        leftController.onSelectExit.AddListener(GetPlatform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (decideIfParent)
+        {
+            decideIfParent = false;
+            if (platform.CompareTag("Platform"))
+            {
+                platformParent = platform;
+                transform.parent = platformParent;
+                transform.localScale = new Vector3 (1/platformParent.localScale.x,1/platformParent.localScale.y,1/platformParent.localScale.z);
+            }
+        }
+    }
+
+    private void GetPlatform(XRBaseInteractable interactable)
+    {
+        platform = interactable.transform;
+        decideIfParent = true;
+    }
+}
--- Assets/Scripts/ChildXR.cs
+++ Assets/Scripts/ChildXR.cs
@@ -5,7 +5,7 @@ public class ChildXR : MonoBehaviour
     public Transform XRObj;
 
     private Vector3 scale;
-    // Start is called before the first frame update
+
     void Awake()
     {
         XRObj = GameObject.FindWithTag("XRObj").transform;
